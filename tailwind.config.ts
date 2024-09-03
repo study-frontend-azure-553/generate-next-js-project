d2775cb2d3e5a028de8c315420309b3c1cc27311
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-03 21:07:34 +0900

    feat: 기본 설정 추가 폰트, 페이지, 컬러 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { colors, fontSize } from "./src/styles/theme";
 
 const config: Config = {
   content: [
@@ -7,11 +8,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize,
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors,
+      fontFamily: {
+        pretendard: ["var(--font-pretendard)"],
+        notoSans: ["var(--font-notoSans)"],
+      },
+      screens: {
+        xs: "360px",
       },
     },
   },
