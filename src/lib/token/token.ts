855ac2884025685c7083ead1559dd487c5436895
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-03 21:24:12 +0900

    feat: token관리 및 쿠키 적용

---
+++ src/lib/token/token.ts
@@ -0,0 +1,23 @@
+import cookie from "../cookie/cookie";
+
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+} from "@/constants/token.contants";
+
+class Token {
+  public getToken(key: string): string | undefined {
+    return cookie.getCookie(key);
+  }
+
+  public setToken(key: string, token: string): void {
+    cookie.setCookie(key, token);
+  }
+
+  public clearToken() {
+    cookie.removeCookie(ACCESS_TOKEN_KEY);
+    cookie.removeCookie(REFRESH_TOKEN_KEY);
+  }
+}
+
+export default new Token();
